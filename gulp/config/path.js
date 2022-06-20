94c799e742d00d6013f7232f2fa30467335f37d3
shkaanas <nastashkirta@gmail.com>
2022-06-20 20:22:45 +0300

    added plugins for images

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -8,12 +8,15 @@ const srcFolder = `./src`; // folder with sources
 export const path = {
   build: {
     js: `${buildFolder}/js/`,
+    images: `${buildFolder}/img/`,
     css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
     files: `${buildFolder}/files/`,
   },
   src: {
     js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
     scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`, // files to copy to build folder
@@ -22,6 +25,7 @@ export const path = {
     js: `${srcFolder}/js/**/*.js`,
     scss: `${srcFolder}/scss/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
     files: `${srcFolder}/files/**/*.*`,
   },
   clean: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ import { html } from './gulp/tasks/html.js';
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
+import { images } from "./gulp/tasks/images.js";
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
@@ -25,10 +26,11 @@ function watcher() {
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
 }
 
 // Основные задачи
-const mainTasks = gulp.parallel(copy, html, scss, js);
+const mainTasks = gulp.parallel(copy, html, scss, js, images);
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
