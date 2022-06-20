0bae7f8ef486900ed1a127305bb0ec3ef5dd5290
shkaanas <nastashkirta@gmail.com>
2022-06-20 16:12:36 +0300

    added module for html

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,4 +1,7 @@
+import fileInclude from 'gulp-file-include';
+
 export const html = () => {
-	return app.gulp.src(app.path.src.html)
-    .pipe(app.gulp.dest(app.path.build.html))
-}
+  return app.gulp.src(app.path.src.html)
+    .pipe(fileInclude())
+    .pipe(app.gulp.dest(app.path.build.html));
+};
