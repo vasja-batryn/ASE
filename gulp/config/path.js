262a41e9fb77b485d156103f8dfb762f7d3efd91
shkaanas <nastashkirta@gmail.com>
2022-06-20 20:00:15 +0300

    added modules for js

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,16 +7,19 @@ const srcFolder = `./src`; // folder with sources
 
 export const path = {
   build: {
+    js: `${buildFolder}/js/`,
     css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
     files: `${buildFolder}/files/`,
   },
   src: {
+    js: `${srcFolder}/js/app.js`,
     scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`, // files to copy to build folder
   },
   watch: {
+    js: `${srcFolder}/js/**/*.js`,
     scss: `${srcFolder}/scss/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
     files: `${srcFolder}/files/**/*.*`,
