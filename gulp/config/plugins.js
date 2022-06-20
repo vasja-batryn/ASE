479b91fc595e89ec9ea6eb728b0a9d29a8bd1ba5
shkaanas <nastashkirta@gmail.com>
2022-06-20 17:05:05 +0300

    plugins for versions and errors

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,10 @@
 import replace from 'gulp-replace'; // Поиск и замена
+import plumber from 'gulp-plumber'; // Обработка ошибок
+import notify from 'gulp-notify'; // Сообщения (подсказки)
 
 // Экспортируем объект
 export const plugins = {
   replace: replace,
+  plumber: plumber,
+  notify: notify,
 };
