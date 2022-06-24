d345ae9cfc75710e70994271b1bc22aecf3a6ca9
shkaanas <nastashkirta@gmail.com>
2022-06-25 00:54:28 +0300

    added product page

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+// for slider at product page
+document.addEventListener( 'DOMContentLoaded', function() {
+    let splide = new Splide( '.splide', {
+        type   : 'loop',
+        drag   : 'free',
+        snap   : true,
+        arrows: false,
+        pagination: false,
+        perPage: 4,
+        breakpoints: {
+            1200: { perPage: 3, gap: '.5rem' },
+            640 : { perPage: 1, gap: '.2rem' },
+          }
+      } );
+      
+      splide.mount();
+  } );
