0067e319f13e39e25a13c63c44899a7d374e35b9
shkaanas <nastashkirta@gmail.com>
2022-06-29 19:56:08 +0300

    added cart

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,56 @@
-import * as flsFunctions from "./modules/functions.js";
+import * as flsFunctions from './modules/functions.js';
 
 flsFunctions.isWebp();
 
 // for slider at product page
-document.addEventListener( 'DOMContentLoaded', function() {
-    if(document.querySelector('.splide')){ // додав проверку на наявність елемента
-      let splide = new Splide( '.splide', {
-        type   : 'loop',
-        drag   : 'free',
-        snap   : true,
-        arrows: false,
-        pagination: false,
-        perPage: 4,
-        breakpoints: {
-            1200: { perPage: 3, gap: '.5rem' },
-            640 : { perPage: 1, gap: '.2rem' },
-          }
-      } );
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelector('.splide')) {
+    // додав проверку на наявність елемента
+    let splide = new Splide('.splide', {
+      type: 'loop',
+      drag: 'free',
+      snap: true,
+      arrows: false,
+      pagination: false,
+      perPage: 4,
+      breakpoints: {
+        1200: { perPage: 3, gap: '.5rem' },
+        640: { perPage: 1, gap: '.2rem' },
+      },
+    });
 
-      splide.mount();
-    }
-  } );
+    splide.mount();
+  }
+});
+
+//For navbar scrolling
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function () {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = '-100px';
+    navbar.style.transition = 'all .6s linear';
+  } else {
+    navbar.style.top = '0px';
+    navbar.style.transition = 'all .6s linear';
+  }
+
+  lastScrollTop = scrollTop;
+});
 
 
 var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
+var msnry = new Masonry(elem, {
   // options
   itemSelector: '.grid-item',
-  columnWidth: 200
+  columnWidth: 200,
 });
 
 // element argument can be a selector string
 //   for an individual element
-var msnry = new Masonry( '.grid', {
+var msnry = new Masonry('.grid', {
   // options
 });
